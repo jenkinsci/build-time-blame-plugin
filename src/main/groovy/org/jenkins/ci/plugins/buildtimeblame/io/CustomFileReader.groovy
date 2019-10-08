2b29aff1b4796a46893bcc44b4a76767d3620773
Luther Tegtmeier <TegtmeierLutherA@JohnDeere.com>
2019-10-08 16:03:33 -0500

    fix: Always close the file reader input stream (#21)
    Closes #18

--- src/main/groovy/org/jenkins/ci/plugins/buildtimeblame/io/CustomFileReader.groovy
+++ src/main/groovy/org/jenkins/ci/plugins/buildtimeblame/io/CustomFileReader.groovy
@@ -3,11 +3,14 @@ package org.jenkins.ci.plugins.buildtimeblame.io
 
 class CustomFileReader {
     public static void eachLineOnlyLF(InputStream inputStream, Closure closure) {
-        Scanner scanner = new Scanner(inputStream);
-        scanner.useDelimiter('\n');
-        while (scanner.hasNext()) {
-            closure.call(scanner.next().trim())
+        try {
+            Scanner scanner = new Scanner(inputStream)
+            scanner.useDelimiter('\n')
+            while (scanner.hasNext()) {
+                closure.call(scanner.next().trim())
+            }
+        } finally {
+            inputStream.close()
         }
-        inputStream.close()
     }
 }
--- src/test/groovy/org/jenkins/ci/plugins/buildtimeblame/io/CustomFileReaderTest.groovy
+++ src/test/groovy/org/jenkins/ci/plugins/buildtimeblame/io/CustomFileReaderTest.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 import static CustomFileReader.eachLineOnlyLF
 
 class CustomFileReaderTest extends Specification {
-    InputStream inputStream = null
+    BufferedInputStream inputStream = null
 
     void cleanup() {
         inputStream.close()
@@ -30,6 +30,45 @@ class CustomFileReaderTest extends Specification {
         1 * mockClosure.call(line2)
         1 * mockClosure.call(line3)
         0 * mockClosure.call(_)
+    }
+
+    def 'should close the input stream'() {
+        given:
+        def mockClosure = Mock(Closure)
+        getTestFile().write("sometext")
+        inputStream = getTestFile().newInputStream()
+
+        when:
+        eachLineOnlyLF(inputStream, mockClosure)
+        inputStream.getText()
+
+        then:
+        def exception = thrown(IOException)
+        exception.message == 'Stream closed'
+    }
+
+    def 'should close the input stream even if there is a failure'() {
+        given:
+        def failureMessage = "Couldn't process item"
+        def mockClosure = {
+            throw new RuntimeException(failureMessage)
+        }
+        getTestFile().write("sometext")
+        inputStream = getTestFile().newInputStream()
+
+        when:
+        eachLineOnlyLF(inputStream, mockClosure)
+
+        then:
+        def exception = thrown(RuntimeException)
+        exception.message == failureMessage
+
+        when:
+        inputStream.getText()
+
+        then:
+        def ioException = thrown(IOException)
+        ioException.message == 'Stream closed'
     }
 
     private static File getTestFile() {
